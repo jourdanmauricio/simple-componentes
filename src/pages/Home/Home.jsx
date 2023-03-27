0f41d8ee25edd14bbf46d02a0adca28fa2a3f3c6
mauricio <jourdanmauricio@gmail.com>
2023-03-27 11:49:32 -0300

    Add text center over image and text alternated width images

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="p-10">
       Recopilación de componentes desarrollados en html y css. Otros también
-      incluyen javascript.
+      incluyen javascript !!.
       <ul className="mt-4">
         {/* <li className="hover:underline list-disc">
           <Link to="/components/modal-html">Ventana Modal. HTML / CSS</Link>
