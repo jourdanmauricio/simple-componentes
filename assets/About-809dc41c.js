16189fc579831b881777f92fe80cdf6aac9bb399
mauricio <jourdanmauricio@gmail.com>
2023-03-26 23:05:19 -0300

    Updates

--- assets/About-809dc41c.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-1202b76e.js";const r=()=>t("h1",{className:"text-center",children:"About"});export{r as default};
---
+++ assets/About-f532d2d9.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-81810e2e.js";const r=()=>t("h1",{className:"text-center",children:"About"});export{r as default};
