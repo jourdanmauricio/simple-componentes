1412cec3a588e9c66383c05d80816c6f3b8d1506
mauricio <jourdanmauricio@gmail.com>
2023-03-26 22:53:30 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'simple-components',
+  base: '/simple-components/',
   plugins: [react()],
   resolve: {
     alias: {
